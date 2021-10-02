1d00c9dfd08f3caf4251e2d68aa43c34e5a3665d
Maksym Kozachuk <56109666+mkozachuk@users.noreply.github.com>
2021-10-02 18:30:15 +0200

    Add files via upload

--- tree-checkbox.component.ts
+++ tree-checkbox.component.ts
@@ -4,24 +4,59 @@ import {MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource} from "
 import {SelectionModel} from "@angular/cdk/collections";
 import {BehaviorSubject} from "rxjs";
 import {DataService} from "../../data.service";
+import {FormControl, FormControlDirective, FormGroup, FormGroupDirective} from "@angular/forms";
 
 /**
- * Node for to-do name
+ * Probably works with OnPush strategy
+ *
+ * In parent component HTML
+ *
+ *   <mat-expansion-panel >
+ <mat-expansion-panel-header>
+ <p>Company Structure</p>
+ </mat-expansion-panel-header>
+ <div class="myClass">
+ <app-tree-checkbox [data]="dummyFrobBackend" [mainControlName]="'structure'"></app-tree-checkbox>
+ </div>
+ </mat-expansion-panel>
+ *
+ * In parent component scss
+ * .myClass{
+  max-height: 200px;
+  overflow: scroll;
+}
+ *
+ * In parent component TS add FormControl and fetch data from service
+ *   dummyFrobBackend: DummyData[] = [];
+ *
+ * add to  onInit()
+ *      this.service.getStructure().subscribe(t => {
+      this.dummyFrobBackend = [t];
+    });
+ *
+ *
+ */
+
+
+
+
+/**
+ * Node type for incoming data
  */
 export interface DummyData {
-  name: string;// ='';
-  level: number;//= 0;
-  children: DummyData[]//; =[];
-  selected?:boolean;
+  id: number;
+  name: string;
+  level: number;
+  children: DummyData[]
 }
 
-/** Flat to-do name node with expandable and level information */
-export class TodonameFlatNode {
+/** Flat name node with expandable and level information */
+export class DummyFlatNode {
+  id: number = 0;
   name: string = '';
   level: number = 1;
   expandable: boolean = false;
 }
-
 
 /**
  * @title Tree with checkboxes
@@ -31,110 +66,76 @@ export class TodonameFlatNode {
   templateUrl: './tree-checkbox.component.html',
   styleUrls: ['./tree-checkbox.component.scss'],
 })
-export class TreeCheckboxComponent  implements OnInit{
-
-  // @Input() data1 : DummyData[] = [{name:'start', level: 0, children:[{name:'start', level: 1, children:[]}]}];
-
-  data1 = 'a';
-fetchedData!: DummyData;
-
-  dataChange = new BehaviorSubject<DummyData[]>([]);
-
-  // get data(): DummyData[] { return this.dataChange.value; }
+export class TreeCheckboxComponent implements OnInit {
 
 
+  @Input() mainControlName!: string;
 
+  rootFormControl!: FormControl;
+  localFormControl = new FormControl();
 
-
-
-  // initialize a private variable _data, it's a BehaviorSubject
-  private _data = new BehaviorSubject<DummyData[]>([{name:'start', level: 0, children:[{name:'start', level: 1, children:[]}]}]);
+  private _data = new BehaviorSubject<DummyData[]>([{
+    id: 0,
+    name: 'start',
+    level: 0,
+    children: [{id: 0, name: 'start', level: 1, children: []}]
+  }]);
 
   // change data to use getter and setter
   @Input()
   set data(value) {
-    // set the latest value for _data BehaviorSubject
     this._data.next(value);
   };
 
   get data() {
-    // get the latest value from _data BehaviorSubject
     return this._data.getValue();
   }
 
-  ngOnInit() {
-    // now we can subscribe to it, whenever input changes,
-    // we will run our grouping logic
-
-  }
-
-
-
-
-
-
-
-
-
   /** Map from flat node to nested node. This helps us finding the nested node to be modified */
-  flatNodeMap = new Map<TodonameFlatNode, DummyData>();
+  flatNodeMap = new Map<DummyFlatNode, DummyData>();
 
   /** Map from nested node to flattened node. This helps us to keep the same object for selection */
-  nestedNodeMap = new Map<DummyData, TodonameFlatNode>();
+  nestedNodeMap = new Map<DummyData, DummyFlatNode>();
 
-  /** A selected parent node to be inserted */
-  selectedParent: TodonameFlatNode | null = null;
+  treeControl!: FlatTreeControl<DummyFlatNode>;
 
-  /** The new name's name */
-  newnameName = '';
+  treeFlattener!: MatTreeFlattener<DummyData, DummyFlatNode>;
 
-  treeControl: FlatTreeControl<TodonameFlatNode>;
-
-  treeFlattener: MatTreeFlattener<DummyData, TodonameFlatNode>;
-
-  dataSource: MatTreeFlatDataSource<DummyData, TodonameFlatNode>;
+  dataSource!: MatTreeFlatDataSource<DummyData, DummyFlatNode>;
 
   /** The selection for checklist */
-  checklistSelection = new SelectionModel<TodonameFlatNode>(true /* multiple */);
+  checklistSelection = new SelectionModel<DummyFlatNode>(true /* multiple */);
 
-  constructor(private service: DataService) {
-    console.log("3" + JSON.stringify(this.data1));
-    // console.log('4'+ JSON.stringify(this.dataSource.data));
-    // this.initialize();
+  form!: FormGroup;
+
+  constructor(public rootForm: FormGroupDirective) {
+  }
+
+  ngOnInit() {
+
+    this.form = this.rootForm.control;
+    this.rootFormControl = this.form.get(this.mainControlName) as FormControl;
+
     this.treeFlattener = new MatTreeFlattener(this.transformer, this.getLevel,
       this.isExpandable, this.getChildren);
-    this.treeControl = new FlatTreeControl<TodonameFlatNode>(this.getLevel, this.isExpandable);
+    this.treeControl = new FlatTreeControl<DummyFlatNode>(this.getLevel, this.isExpandable);
     this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-    console.log("4" + JSON.stringify(this.data1));
-    console.log('5'+ JSON.stringify(this.dataSource?.data));
-    // this.dataSource.data = this.data1;
-    // _database.dataChange.subscribe(data => {
-    //   this.dataSource.data = this.data;
-    // });
 
     this._data
       .subscribe(x => {
         this.dataSource.data = this.data;
       });
-
-    // this.dataChange.subscribe(data => {
-    //   this.dataSource.data = data;
-    //   console.log("6" + JSON.stringify(this.data1));
-    //   console.log('7'+ JSON.stringify(this.dataSource?.data));
-    // });
-    console.log("8" + JSON.stringify(this.data1));
-    console.log('9'+ JSON.stringify(this.dataSource?.data));
   }
 
-  getLevel = (node: TodonameFlatNode) => node.level;
+  getLevel = (node: DummyFlatNode) => node.level;
 
-  isExpandable = (node: TodonameFlatNode) => node.expandable;
+  isExpandable = (node: DummyFlatNode) => node.expandable;
 
   getChildren = (node: DummyData): DummyData[] => node.children;
 
-  hasChild = (_: number, _nodeData: TodonameFlatNode) => _nodeData.expandable;
+  hasChild = (_: number, _nodeData: DummyFlatNode) => _nodeData.expandable;
 
-  hasNoContent = (_: number, _nodeData: TodonameFlatNode) => _nodeData.name === '';
+  hasNoContent = (_: number, _nodeData: DummyFlatNode) => _nodeData.name === '';
 
   /**
    * Transformer to convert nested node to flat node. Record the nodes in maps for later use.
@@ -143,17 +144,18 @@ fetchedData!: DummyData;
     const existingNode = this.nestedNodeMap.get(node);
     const flatNode = existingNode && existingNode.name === node.name
       ? existingNode
-      : new TodonameFlatNode();
+      : new DummyFlatNode();
+    flatNode.id = node.id;
     flatNode.name = node.name;
     flatNode.level = level;
     flatNode.expandable = !!node.children?.length;
     this.flatNodeMap.set(flatNode, node);
     this.nestedNodeMap.set(node, flatNode);
     return flatNode;
-  }
+  };
 
-  /** Whether all the descendants of the node are selected. */
-  descendantsAllSelected(node: TodonameFlatNode): boolean {
+  /** Checking if all nodes are selected */
+  descendantsAllSelected(node: DummyFlatNode): boolean {
     const descendants = this.treeControl.getDescendants(node);
     const descAllSelected = descendants.length > 0 && descendants.every(child => {
       return this.checklistSelection.isSelected(child);
@@ -161,47 +163,53 @@ fetchedData!: DummyData;
     return descAllSelected;
   }
 
-  /** Whether part of the descendants are selected */
-  descendantsPartiallySelected(node: TodonameFlatNode): boolean {
+  /** Checking if any node is selected  */
+  descendantsPartiallySelected(node: DummyFlatNode): boolean {
     const descendants = this.treeControl.getDescendants(node);
     const result = descendants.some(child => this.checklistSelection.isSelected(child));
     return result && !this.descendantsAllSelected(node);
   }
 
   /** Toggle the to-do name selection. Select/deselect all the descendants node */
-  todonameSelectionToggle(node: TodonameFlatNode): void {
+  dummyFlatNodeSelectionToggle(node: DummyFlatNode): void {
     this.checklistSelection.toggle(node);
     const descendants = this.treeControl.getDescendants(node);
+
     this.checklistSelection.isSelected(node)
       ? this.checklistSelection.select(...descendants)
       : this.checklistSelection.deselect(...descendants);
 
     // Force update for the parent
-    descendants.forEach(child => this.checklistSelection.isSelected(child));
+    descendants.forEach((child) => {
+      this.checklistSelection.isSelected(child);
+    });
+
     this.checkAllParentsSelection(node);
 
-    console.log('selection togle' + JSON.stringify(this.checklistSelection))
+    this.rootFormControl.setValue(this.checklistSelection.selected); //form transfer
+
   }
 
-  /** Toggle a leaf to-do name selection. Check all the parents to see if they changed */
-  todoLeafnameSelectionToggle(node: TodonameFlatNode): void {
+  /** Toggle a leaf selection. Check all the parents to see if they changed */
+  dummyFlatNodeLeafSelectionToggle(node: DummyFlatNode): void {
     this.checklistSelection.toggle(node);
     this.checkAllParentsSelection(node);
-    console.log('todoLeafnameSelectionToggle' + JSON.stringify(this.checklistSelection))
+
+    this.rootFormControl.setValue(this.checklistSelection.selected); //form transfer
   }
 
   /* Checks all the parents when a leaf node is selected/unselected */
-  checkAllParentsSelection(node: TodonameFlatNode): void {
-    let parent: TodonameFlatNode | null = this.getParentNode(node);
+  checkAllParentsSelection(node: DummyFlatNode): void {
+    let parent: DummyFlatNode | null = this.getParentNode(node);
     while (parent) {
       this.checkRootNodeSelection(parent);
       parent = this.getParentNode(parent);
     }
-    console.log('checkAllParentsSelection' + JSON.stringify(this.checklistSelection))
+
   }
 
   /** Check root node checked state and change it accordingly */
-  checkRootNodeSelection(node: TodonameFlatNode): void {
+  checkRootNodeSelection(node: DummyFlatNode): void {
     const nodeSelected = this.checklistSelection.isSelected(node);
     const descendants = this.treeControl.getDescendants(node);
     const descAllSelected = descendants.length > 0 && descendants.every(child => {
@@ -212,13 +220,10 @@ fetchedData!: DummyData;
     } else if (!nodeSelected && descAllSelected) {
       this.checklistSelection.select(node);
     }
-    console.log('checkRootNodeSelection' + JSON.stringify(this.checklistSelection))
-
-    console.log("descAllSelected" + descAllSelected);
   }
 
   /* Get the parent node of a node */
-  getParentNode(node: TodonameFlatNode): TodonameFlatNode | null {
+  getParentNode(node: DummyFlatNode): DummyFlatNode | null {
     const currentLevel = this.getLevel(node);
 
     if (currentLevel < 1) {
@@ -236,7 +241,6 @@ fetchedData!: DummyData;
     }
     return null;
   }
-
 
 
 }
